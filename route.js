756a4b951d22c20d9dc1978d969dfdac6fdde735
Shahul VM <shahul.vm88@gmail.com>
2019-02-27 12:15:48 +0530

    mock server basic

---
+++ route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const user = require('./user.json');
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use(express.static("dist"));
+app.get("/user", (req, res) => {
+    res.send(user);
+});
+app.listen(port);
+
